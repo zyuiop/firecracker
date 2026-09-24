27f6b0bd9a08f661b86864e9377b3d2a65411d38
Louis Vialar <louis.vialar@gmail.com>
2026-09-24 20:47:09 +0200

    mmio: remove data len patch

--- src/vmm/src/devices/virtio/transport/mmio.rs
+++ src/vmm/src/devices/virtio/transport/mmio.rs
@@ -227,7 +227,7 @@ impl MmioTransport {
 impl BusDevice for MmioTransport {
     fn read(&mut self, base: u64, offset: u64, data: &mut [u8]) {
         match offset {
-            0x00..=0xff if data.len() == 4 || data.len() == 8 => {
+            0x00..=0xff if data.len() == 4 => {
                 let v = match offset {
                     0x0 => MMIO_MAGIC_VALUE,
                     0x04 => MMIO_VERSION,
